287a20d8c3635a3872e9e0174cb65cb0f49eb930
RaiBnod <binodrayee@gmail.com>
2021-11-16 11:20:01 +0545

    Change into centos

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-setsid ./rubix-bios -p 1615 -g /data/rubix-bios -d data -c config -a apps --prod --auth --device-type amd64 --token $(eval echo "$@") > output.txt &
-exec /sbin/init
+
+setsid ./rubix-bios -p 1615 -g /data/rubix-bios -d data -c config -a apps --prod --auth --device-type amd64 --token $(eval echo "$@") &
+exec /usr/sbin/init
